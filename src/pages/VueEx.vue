824c70d391bd667fb8758ccc0fcb6c3c51e28e28
kkkmms <jam0626@naver.com>
2023-04-09 19:09:37 +0900

    real first

---
+++ src/pages/VueEx.vue
@@ -0,0 +1,35 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name:'',
+  components:{},
+  data(){
+    return{
+
+    }
+  },
+  computed:{
+
+  },
+  watch:{
+
+  },
+
+  title:"Vue를 재미있게 공부해봅시다!",
+
+  mounted(){
+
+  },
+  methods:{
+
+  }
+
+}
+</script>
+
+<style>
+
+</style>
